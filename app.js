77e79eaf29586cbbcd19e0a96c9949ee4be65185
pujith20 <pujithkumarpamujula.16@gmail.com>
2025-05-08 23:52:13 +0530

    Latest Backend Changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true, // Allow cookies/authorization headers
   })
 );
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 // WebSocket setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,    
   },
 });
